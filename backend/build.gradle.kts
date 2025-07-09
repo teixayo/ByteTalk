4d042d249f14ff79ce3f6a0f46743c3031f302c0
teixayo <teixayo@gmail.com>
2025-07-09 08:44:29 +0330

    Cleanup gradle

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -17,53 +17,37 @@ repositories {
     }
 }
 
-dependencies { 
-    implementation(project(":shared"))
+dependencies {
 
-    implementation("org.yaml:snakeyaml:2.2")
-    implementation("io.netty:netty-transport-native-epoll:4.2.1.Final")
-    implementation("io.netty:netty-transport-native-epoll:4.2.1.Final:linux-x86_64")
-    implementation("io.netty:netty-transport-native-epoll:4.2.1.Final:linux-aarch_64")
+    implementation(libs.netty.codec)
+    implementation(libs.netty.codec.http)
+    implementation(libs.netty.handler)
+    implementation(libs.netty.transport.native.epoll)
+    implementation(variantOf(libs.netty.transport.native.epoll) { classifier("linux-x86_64") })
+    implementation(variantOf(libs.netty.transport.native.epoll) { classifier("linux-aarch_64") })
+    implementation(libs.netty.transport.native.iouring)
+    implementation(variantOf(libs.netty.transport.native.iouring) { classifier("linux-x86_64") })
+    implementation(variantOf(libs.netty.transport.native.iouring) { classifier("linux-aarch_64") })
+    implementation(libs.netty.transport.native.kqueue)
+    implementation(variantOf(libs.netty.transport.native.kqueue) { classifier("osx-x86_64") })
+    implementation(variantOf(libs.netty.transport.native.kqueue) { classifier("osx-aarch_64") })
+    implementation(libs.mongodb)
+    implementation(libs.elasticsearch)
+    implementation(libs.jedis)
+    implementation(libs.json)
+    implementation(libs.slf4j)
+    implementation(libs.bundles.log4j)
+    implementation(libs.jegl)
+    implementation(libs.jwt)
+    implementation(libs.snakeyaml)
 
-    implementation("io.netty:netty-transport-native-kqueue:4.2.1.Final")
-    implementation("io.netty:netty-transport-native-kqueue:4.2.1.Final:osx-x86_64")
-    implementation("io.netty:netty-transport-native-kqueue:4.2.1.Final:osx-aarch_64")
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
 
-    implementation("io.netty:netty-transport-native-io_uring:4.2.1.Final")
-    implementation("io.netty:netty-transport-native-io_uring:4.2.1.Final:linux-x86_64")
-    implementation("io.netty:netty-transport-native-io_uring:4.2.1.Final:linux-aarch_64")
-
-    implementation("io.netty:netty-handler:4.2.1.Final")
-    implementation("io.netty:netty-codec-http:4.2.1.Final")
-    implementation("io.netty:netty-codec:4.2.1.Final")
-
-    implementation("io.lettuce:lettuce-core:6.3.0.RELEASE")
-
-    implementation("org.mongodb:mongodb-driver-sync:4.11.0")
-    implementation("co.elastic.clients:elasticsearch-java:8.17.0")
-
-    implementation("org.json:json:20240303")
-
-    implementation("org.slf4j:slf4j-api:2.0.9")
-    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0")
-    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
-
-    implementation("com.github.teixayo:JEGL:v1.0")
-    implementation("com.auth0:java-jwt:4.4.0")
-
-    compileOnly("org.projectlombok:lombok:1.18.34")
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
-    testCompileOnly("org.projectlombok:lombok:1.18.34")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
-
-    implementation("redis.clients:jedis:5.1.0")
-
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-
-    testImplementation("org.testcontainers:testcontainers:1.21.0")
-    testImplementation("org.testcontainers:junit-jupiter:1.21.0")
-    testImplementation("org.testcontainers:mongodb:1.21.0")
+    testImplementation(libs.junit)
+    testImplementation(libs.bundles.testcontainer)
+    testAnnotationProcessor(libs.lombok)
+    testCompileOnly(libs.lombok)
 }
 
 tasks.test {
--- shared/src/main/java/me/teixayo/bytetalk/Test.java
+++
@@ -1,8 +0,0 @@
-package me.teixayo.bytetalk;
-
-public class Test {
-
-    public static void main(String[] args) {
-
-    }
-}
