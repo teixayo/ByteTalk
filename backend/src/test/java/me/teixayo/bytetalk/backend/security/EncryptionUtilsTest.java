f8bf5643687bc2e053f813416486775988de6f76
teixayo <teixayo@gmail.com>
2025-07-09 08:58:29 +0330

    Test testing

--- backend/src/test/java/me/teixayo/bytetalk/backend/security/EncryptionUtilsTest.java
+++ backend/src/test/java/me/teixayo/bytetalk/backend/security/EncryptionUtilsTest.java
@@ -8,7 +8,7 @@ public class EncryptionUtilsTest {
 
     @Test
     void testEncryptMethod() {
-        assertEquals(Crypto.encryptSHA256("Test"),"532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e251");
+        assertEquals(Crypto.encryptSHA256("Test"),"532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25");
 
     }
 
